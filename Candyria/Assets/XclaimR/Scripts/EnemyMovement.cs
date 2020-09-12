5abaafb35eed2c7d46e3543a73db7d2e03175b7d
nishanthkorupolu <korupolu.nishanth@gmail.com>
2020-09-13 00:02:36 +0530

    Camera, Player,Enemy Movement Added

---
+++ Candyria/Assets/XclaimR/Scripts/EnemyMovement.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+
+    public float speed;
+    public bool MoveRight = false;
+
+    // Use this for initialization
+    void Update()
+    {
+        // Use this for initialization
+        if (MoveRight)
+        {
+            transform.Translate(2 * Time.deltaTime * speed, 0, 0);
+            transform.localScale = new Vector2(-0.35779f, 0.35779f);
+        }
+        else
+        {
+            transform.Translate(-2 * Time.deltaTime * speed, 0, 0);
+            transform.localScale = new Vector2(0.35779f, 0.35779f);
+        }
+    }
+    void OnTriggerEnter2D(Collider2D trig)
+    {
+        if (trig.gameObject.CompareTag("Turn"))
+        {
+            if (MoveRight)
+            {
+                MoveRight = false;
+            }
+            else
+            {
+                MoveRight = true;
+            }
+        }
+    }
+}
---
+++ Candyria/Assets/XclaimR/Scripts/Player.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Player : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float health = 100;
+
+    void Update()
+    {
+        if(transform.position.y <= -20)
+        {
+            TakeDamage(Mathf.Infinity);
+        }
+        
+    }
+
+    public void TakeDamage(float damage)
+    {
+        health -= damage;
+        if(health <= 0)
+        {
+            Debug.Log("Player Dead");
+            Destroy(gameObject);
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Enemy")
+        {
+            TakeDamage(20);
+        }
+    }
+
+
+}
